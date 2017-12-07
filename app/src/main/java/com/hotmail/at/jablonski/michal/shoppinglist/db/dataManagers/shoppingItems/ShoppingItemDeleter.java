e03b01c3e8a0041a8bfaa17829ba07db0f5948e7
kofcio94 <kofcio94@gmail.com>
2017-12-07 23:23:38 +0100

    First DB query moved to RxJava

--- app/src/main/java/com/hotmail/at/jablonski/michal/shoppinglist/db/dataManagers/shoppingItems/ShoppingItemDeleter.java
+++ app/src/main/java/com/hotmail/at/jablonski/michal/shoppinglist/db/dataManagers/shoppingItems/ShoppingItemDeleter.java
@@ -1,11 +1,16 @@
 package com.hotmail.at.jablonski.michal.shoppinglist.db.dataManagers.shoppingItems;
 
-import android.os.AsyncTask;
-
 import com.hotmail.at.jablonski.michal.shoppinglist.db.dao.ChildDao;
 import com.hotmail.at.jablonski.michal.shoppinglist.ui.details.listAdapter.ShoppingItem;
 
-public class ShoppingItemDeleter extends AsyncTask<Void, Void, Void> {
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+public class ShoppingItemDeleter {
 
     private ShoppingItem shoppingItem;
     private ShoppingItemDeleterListener listener;
@@ -15,16 +20,36 @@ public class ShoppingItemDeleter extends AsyncTask<Void, Void, Void> {
         this.listener = listener;
     }
 
-    @Override
-    protected Void doInBackground(Void... voids) {
-        ChildDao dao = new ChildDao();
-        dao.delete(shoppingItem.toChildEntity());
-        return null;
-    }
+    public void execute() {
+        Observable.just(shoppingItem)
+                .map((Function<ShoppingItem, Void>) shoppingItem -> {
+                    ChildDao dao = new ChildDao();
+                    dao.delete(shoppingItem.toChildEntity());
+                    return null;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Void>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
 
-    @Override
-    protected void onPostExecute(Void aVoid) {
-        listener.deleted();
+                    }
+
+                    @Override
+                    public void onNext(Void value) {
+                        listener.deleted();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
     public interface ShoppingItemDeleterListener {
